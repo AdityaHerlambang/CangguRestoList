20dbe6b412cac7952aad5dba5f1027aa436d4c7e
Aditya Herlambang <herlambangadt@gmail.com>
2019-04-17 12:52:31 +0800

    Changed Concept to Restaurant, Add Model and Data

---
+++ app/src/main/java/com/herlambang/canggurestaurants/data/StartupData.java
@@ -0,0 +1,9 @@
+package com.herlambang.canggurestaurants.data;
+
+public class StartupData {
+
+    public static String[][] data = new String[][]{
+            {"Suksema Sushi Bar", "Sushi and Japanese Startup", "9am - 10pm", ""}
+    };
+
+}
